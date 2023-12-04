66358aede8c4202417c7009bf3d25e128b4c0352
vaibhavrajpianist <vaibhavrajpianist@gmail.com>
2023-12-04 17:14:29 +0530

    added Dashboard component

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
 function Dashboard() {
+  function handleLogout() {
+    localStorage.removeItem('token');
+    window.location.reload();
+  }
   return (
     <div>
         <h1>Welcome to Dashboard</h1>
+        <button onClick={handleLogout}>Log Out</button>
     </div>
   )
 }
