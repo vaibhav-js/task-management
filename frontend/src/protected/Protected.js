33434e12fc6bb873789858887182742dc12f0e73
vaibhavrajpianist <vaibhavrajpianist@gmail.com>
2023-12-11 20:08:39 +0530

    added multiple conditions to handle protected route

--- frontend/src/protected/Protected.js
+++ frontend/src/protected/Protected.js
@@ -1,6 +1,6 @@
 import { Navigate } from "react-router-dom";
 const Protected = ({children}) => {
-  if (!localStorage.getItem('token')) {
+  if (!localStorage.getItem('token') || !localStorage.getItem('name') || !localStorage.getItem('role')) {
     return <Navigate to="/" replace />;
   }
   return children;
