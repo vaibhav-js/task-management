bbbb8e12d4857eb89a9fd4924c7aa21e4dac9104
vaibhavrajpianist <vaibhavrajpianist@gmail.com>
2023-12-11 20:11:31 +0530

    removing items from localstorage if user not authenticated

--- frontend/src/protected/Protected.js
+++ frontend/src/protected/Protected.js
@@ -1,6 +1,9 @@
 import { Navigate } from "react-router-dom";
 const Protected = ({children}) => {
   if (!localStorage.getItem('token') || !localStorage.getItem('name') || !localStorage.getItem('role')) {
+    localStorage.removeItem('token')
+    localStorage.removeItem('name')
+    localStorage.removeItem('role')
     return <Navigate to="/" replace />;
   }
   return children;
