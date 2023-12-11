91489897a9a7ccbcaccf844db58b2ee70bdbe359
vaibhavrajpianist <vaibhavrajpianist@gmail.com>
2023-12-11 13:27:42 +0530

    updated Logout functionality in Client component

--- frontend/src/components/Client.js
+++ frontend/src/components/Client.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import TicketForm from './TicketForm';
 import TicketTable from './TicketTable';
+import swal from 'sweetalert';
+import axios from 'axios';
 
 function Client() {
-  function handleLogout() {
-    localStorage.removeItem('token');
-    localStorage.removeItem('name');
-    localStorage.removeItem('role');
-    window.location.reload();
+  async function handleLogout() {
+    try {
+      const data = {
+        "token": localStorage.getItem('token')
+      }
+      const response = await axios.post('http://localhost:8080/logout', data);
+      if (response.data.error === false) {
+        localStorage.removeItem('token');
+        localStorage.removeItem('name');
+        localStorage.removeItem('role');
+        window.location.reload();
+      } else {
+        await swal("Error", response.data.message, "error");
+      }
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   return (
