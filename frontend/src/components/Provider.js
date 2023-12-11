af35d9c4d19945544f8149173ae42c4dc7381393
vaibhavrajpianist <vaibhavrajpianist@gmail.com>
2023-12-11 13:28:03 +0530

    updated Logout functionality in Provider component

--- frontend/src/components/Provider.js
+++ frontend/src/components/Provider.js
@@ -1,14 +1,28 @@
 import React from 'react'
 import Form from './Form';
 import TicketTable from './TicketTable';
+import axios from 'axios';
+import swal from 'sweetalert';
 
 function Provider() {
 
-  function handleLogout() {
-      localStorage.removeItem('token');
-      localStorage.removeItem('name');
-      localStorage.removeItem('role');
-      window.location.reload();
+  async function handleLogout() {
+    try {
+      const data = {
+        "token": localStorage.getItem('token')
+      }
+      const response = await axios.post('http://localhost:8080/logout', data);
+      if (response.data.error === false) {
+        localStorage.removeItem('token');
+        localStorage.removeItem('name');
+        localStorage.removeItem('role');
+        window.location.reload();
+      } else {
+        await swal("Error", response.data.message, "error");
+      }
+    } catch (error) {
+      console.error(error);
+    }
   }
   return (
     <div>
