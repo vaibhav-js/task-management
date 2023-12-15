ed1af48f2b4de77db8b8c861076391c4bf45fee6
vaibhavrajpianist <vaibhavrajpianist@gmail.com>
2023-12-15 15:36:12 +0530

    imported styles

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Client from './Client';
 import Provider from './Provider';
+import '../styles/Dashboard.css'
 
 function Dashboard() {
   const role = localStorage.getItem('role');
   return (
-    <div>
+    <div className='dashboard'>
       {
         role === 'Client' ? <Client /> : (role === 'Provider') ? <Provider /> : <></>
       }
