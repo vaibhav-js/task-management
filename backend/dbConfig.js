52a4f444a18c3cd788647deffc7c4b0b5ce2e1df
vaibhavrajpianist <vaibhavrajpianist@gmail.com>
2023-12-16 21:12:42 +0530

    used environment variables

--- backend/dbConfig.js
+++ backend/dbConfig.js
@@ -1,11 +1,12 @@
 const { Pool } = require('pg');
+require('dotenv').config()
 
 const pool = new Pool({
-    host: 'localhost',
-    port: 5432,
-    database: 'taskmanagement',
-    user: 'postgres',
-    password: 'admin'
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    database: process.env.DATABASE_DATABASENAME,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD
 });
 
 module.exports = pool;
