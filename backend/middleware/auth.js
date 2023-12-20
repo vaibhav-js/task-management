260caf9f618ba22ecab63ab1ca029a5d0d9162ae
vaibhavrajpianist <vaibhavrajpianist@gmail.com>
2023-12-20 19:52:36 +0530

    added middleware for authentication and authorization of the user

---
+++ backend/middleware/auth.js
@@ -0,0 +1,26 @@
+const pool = require('../dbConfig')
+
+const auth = async (request, response, next) => {
+    try {
+        const token = request.headers.authorization;
+
+        if (!token) {
+            response.send({ message: 'Unauthorized: Missing token'});
+            return;
+        }
+        
+        const user = await pool.query('select * from users where token = $1', [token.replace("Bearer ", "")]);
+        
+        if (user.rowCount === 0) {
+            response.send({ message: 'Unauthorized: Invalid token'});
+            return;
+        }
+        request.user = user.rows[0];
+        next();
+    } catch (error) {
+        console.error(error)
+        response.send({ message: 'Something went wrong..'});
+    }
+}
+
+module.exports = auth;
