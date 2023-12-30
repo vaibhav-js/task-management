3032f8cc25c4b46330db30c6273d68857ea16e99
vaibhavrajpianist <vaibhavrajpianist@gmail.com>
2023-12-30 12:43:45 +0530

    serving static file

--- backend/index.js
+++ backend/index.js
@@ -2,35 +2,41 @@ const express = require('express');
 const pool = require('./config/dbConfig.js')
 const userRoutes = require('./routers/user.js');
 const ticketRoutes = require('./routers/ticket.js');
+const path = require('path')
+const fs = require('fs')
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+const buildPath = path.resolve(__dirname, '../frontend/build');
+app.use(express.static(buildPath));
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
 });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(buildPath, 'index.html'));
+});
+
 app.use('/users', userRoutes);
 app.use('/tickets', ticketRoutes);
 
 pool.query('SELECT NOW()', (err, res) => {
-    if (err) {
-      console.error('Error connecting to the database :(', err);
-    } else {
-      console.log('Connected to the database :)');
-    }
+  if (err) {
+    console.error('Error connecting to the database :(', err);
+  } else {
+    console.log('Connected to the database :)');
+  }
 });
 
-app.get('/', (request, response) => {
-    response.send({message: "Welcome"});
-})
-
-app.listen(8080, () => {
-    console.log('server running on port 8080')
+app.listen(PORT, () => {
+  console.log('server running on port', PORT)
 });
